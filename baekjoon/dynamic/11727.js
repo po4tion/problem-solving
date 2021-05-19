9e87843af1c46010fc70d3dbd392550b5d7a7030
DongKyu <po4tion0429@gmail.com>
2021-05-20 01:42:17 +0900

    11727번 2xn 타일링2

--- baekjoon/dynamic/11727.js
+++ baekjoon/dynamic/11727.js
@@ -2,10 +2,18 @@
 
 const rl = require('readline').createInterface(process.stdin, process.stdout);
 
-let input = '';
+let input = 0,
+  v = 1;
 
 rl.on('line', (line) => {
-  input = line;
+  input = +line;
 }).on('close', () => {
-  console.log(input);
+  const answer = [, 1];
+
+  for (let i = 2; i <= input; i++) {
+    answer[i] = (answer[i - 1] * 2 + v) % 10007;
+    v *= -1;
+  }
+
+  console.log(answer[input]);
 });
